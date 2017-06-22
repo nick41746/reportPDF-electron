b8e8102dbdbb3faa7b189876d0ead4a082e33d94
Chawin Aiemvaravutigul <nick41746@hotmail.com>
2017-06-22 21:06:03 +0700

    fix page

--- src/index.js
+++ src/index.js
@@ -1,39 +1,10 @@
-HTMLToPDF = require('html5-to-pdf')
-htmlToPDF = new HTMLToPDF ({
-  inputPath: 'test/testpdf.html',
-  include: [{
-    'type': 'css',
-    'filePath': 'test/style/main.css'
-  }],
-  outputPath: 'output.pdf',
-})
+PDFDocument = require('pdfkit')
+fs = require('fs')
 
-htmlToPDF.build ((error) => {
-  if (error) {
-    throw err
-  }
-})
-// const fs = require('fs');
-// const pdf = require('html-pdf');
+doc = new PDFDocument
 
-// var content;
-// // First I want to read the file
-// fs.readFile('src/index.html', 'utf8', function read(err, data) {
-//     if (err) {
-//         throw err;
-//     }
-//     content = data;
+doc.pipe(fs.createWriteStream('output.pdf'))
 
-//     // Invoke the next step here however you like
-//   //  console.log(content);   // Put all of the code here (not the best solution)
-//     processFile(content);          // Or put the next step in a function and invoke it
-// });
+doc.image('src/ittp.png')
 
-// function processFile(content) {
-//   const options = { format: 'Letter' };
-
-//   pdf.create(content, null).toFile('./files/files.pdf', (err, res) => {
-//   if (err) return console.log(err);
-//     console.log(res); // { filename: '/app/businesscard.pdf' }
-//   });
-// }
+doc.end()
